488ccf431c1c40b610fb0c07c64ddea38026ab71
mgambrell <mgambrell@users.noreply.github.com>
2024-04-12 01:15:53 -0400

    fix my dll so it always exports

--- dll.cpp
+++ dll.cpp
@@ -1,10 +1,17 @@
+#include "WebKit/ea/Api/EAWebKit/include/EAWebKit/DLLInterface.h"
+
 namespace EA::WebKit
 {
 	class EAWebKitLib;
 }
 extern "C" EA::WebKit::EAWebKitLib* CreateEAWebkitInstance(void);
 
-extern "C" EA::WebKit::EAWebKitLib* WrapCreateEAWebkitInstance(void)
+//can't figure out how to get the headers to work to do this.. oh well
+#if defined(_MSC_VER) || defined(EA_PLATFORM_PS4) || defined(EA_PLATFORM_PS5)
+#define EXPORTME __declspec(dllexport)
+#endif
+
+extern "C" EXPORTME EA::WebKit::EAWebKitLib* WrapCreateEAWebkitInstance(void)
 {
 	return CreateEAWebkitInstance();
 }
